95feb66980ee5ea74c13ae4a4dcd015027ff729d
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-10-09 17:43:43 -0600

    migrated books, passing tests

---
+++ app/models/book.rb
@@ -0,0 +1,4 @@
+class Book < ApplicationRecord
+  validates_presence_of :title, :pages, :year
+
+end
---
+++ spec/models/book_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Book, type: :model do
+  describe 'Validations' do
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:pages) }
+  it { should validate_presence_of(:year) }
+end
+
+# describe 'Relationship' do
+#   it { should have_many(:songs) }
+# end
+end
