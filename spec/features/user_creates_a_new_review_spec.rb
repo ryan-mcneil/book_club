073cbacc64c5f7e60a6928fac8165cc975c946ff
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-10-15 19:19:59 -0600

    added link to new review

--- spec/features/user_creates_a_new_review_spec.rb
+++ spec/features/user_creates_a_new_review_spec.rb
@@ -10,11 +10,11 @@ describe "user creates a new review" do
           pages: 200,
           year: 2000)
 
-        # visit book_path(@book_1)
-        # click_link "Create a New Review"
-        #
-        # expect(current_path).to eq(new_book_review_path(@book_1))
-        visit new_book_review_path(@book_1)
+        visit book_path(@book_1)
+        click_link "Create a New Review"
+
+        expect(current_path).to eq(new_book_review_path(@book_1))
+        # visit new_book_review_path(@book_1)
 
         fill_in "username", with: "New Name"
         fill_in "review[title]", with: "New Title"
